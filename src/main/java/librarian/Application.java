ef79524404d8cd2db5b50f2f80c32f2dd82cf5a9
JK <jkktom@gmail.com>
2025-03-17 21:49:33 +0900

    WIP from array to List

--- src/main/java/librarian/Application.java
+++ src/main/java/librarian/Application.java
@@ -1,24 +1,23 @@
 package librarian;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-
-        LibraryMember[] membersArr = new LibraryMember[]{
-                new LibraryMember("Jake", "LM001", 3),
-                new LibraryMember("Lyn", "LM002", 3),
-                new LibraryMember("Kyle", "LM003", 3),
-                new StudentMember("Alan", "LM004", 3, 2),
-                new StudentMember("Bob", "LM005", 3, 2),
-        };
-        List<String> members = new ArrayList(Arrays.asList(membersArr));
+        List<LibraryMember> members = new ArrayList<>();
+        
+        // Add initial members
+        addMemberAndPrint(members, new LibraryMember("Jake", "LM001", 3));
+        addMemberAndPrint(members, new LibraryMember("Lyn", "LM002", 3));
+        addMemberAndPrint(members, new LibraryMember("Kyle", "LM003", 3));
+        addMemberAndPrint(members, new StudentMember("Alan", "SM004", 3, 2));
+        addMemberAndPrint(members, new StudentMember("Bob", "SM005", 3, 2));
 
         int totalMembers = members.size();
         System.out.println("Total members: " + totalMembers);
+
         // Scanner를 이용한 회원 등록
         Scanner scanner = new Scanner(System.in);
         System.out.print("회원 이름을 입력하세요: ");
@@ -28,28 +27,25 @@ public class Application {
         boolean isStudent = scanner.nextLine().toUpperCase().equals("Y");
 
         LibraryMember newMember;
-        newMember = new LibraryMember(
-            name,
-            generateMemberId(isStudent, totalMembers),
-            (isStudent ? 5 : 3)
-        );
-        members.add(newMember.toString());
+        if (isStudent) {
+            newMember = new StudentMember(name, generateMemberId(isStudent, totalMembers), 3, 2);
+        } else {
+            newMember = new LibraryMember(name, generateMemberId(isStudent, totalMembers), 3);
+        }
+        addMemberAndPrint(members, newMember);
+    }
+
+    private static void addMemberAndPrint(List<LibraryMember> members, LibraryMember member) {
+        members.add(member);
         System.out.println("회원이 등록되었습니다:");
-        System.out.println("이름: " + newMember.getName());
-        System.out.println("ID: " + newMember.getMemberShipId());
-        System.out.println("대출 한도: " + newMember.getBorrowLimit());
-
-
-    };
+        System.out.println("이름: " + member.getName());
+        System.out.println("ID: " + member.getMemberShipId());
+        System.out.println("대출 한도: " + member.getBorrowLimit());
+    }
 
     public static String generateMemberId(boolean isStudent, int totalMembers) {
-        int memberId = 0;
-        memberId = totalMembers + 1;
-        String memberIdStr = memberId + "";
-        if (isStudent) {
-            return "SM" + memberIdStr;
-        } else {
-            return "LM" + memberIdStr;
-        }
-    };
+        int memberId = totalMembers + 1;
+        String memberIdStr = String.valueOf(memberId);
+        return (isStudent ? "SM" : "LM") + memberIdStr;
+    }
 }
